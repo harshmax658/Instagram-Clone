1d6253ad220c78b716549ed0f7239d290c254f09
harshmax658 <harshmax658@gmail.com>
2022-01-26 16:14:59 +0530

    update folder structure

--- frontend/src/components/IMG/imageStyle.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Image = styled.img`
-  width: ${({ width }) => (width ? width : "100px")};
-  height: ${({ height }) => (height ? height : "100px")};
-`;
