c90557f88be9aed7743fe04b06f3f129f1761a5d
harshmax658 <harshmax658@gmail.com>
2022-01-26 13:22:49 +0530

    login Signup Page phase 2

--- frontend/src/pages/LoginSignupPage.jsx
+++ frontend/src/pages/LoginSignupPage.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-import { Container } from "./LoginSignupPage_style";
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  InstaPhoneImage,
+  Section,
+  RenderdImages,
+} from "./LoginSignupPage_style";
+import ImageComponent from "../components/IMG/ImageComponent";
 
 const LoginSignupPage = () => {
+  const [imageNumber, setImageNumber] = useState([1, 2, 3, 4, 5]);
+
   return (
     <>
-      <Container></Container>
+      <Container>
+        <Section>
+          <InstaPhoneImage image="https://www.instagram.com/static/images/homepage/home-phones@2x.png/9364675fb26a.png">
+            <RenderdImages>
+              {imageNumber.map((data) => (
+                <ImageComponent
+                  className="hide"
+                  key={data}
+                  image={`/images/screenshot${data}.jpg`}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              ))}
+            </RenderdImages>
+          </InstaPhoneImage>
+        </Section>
+      </Container>
     </>
   );
 };
--- frontend/src/pages/LoginSignupPage_style.js
+++ frontend/src/pages/LoginSignupPage_style.js
@@ -1,6 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #fafafa;
   height: 100%;
+`;
+export const Section = styled.section`
+  border: 2px solid;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const InstaPhoneImage = styled.div`
+  background-image: ${({ image }) => `url(${image})`};
+  background-size: cover;
+  width: 400px;
+  height: 600px;
+`;
+export const RenderdImages = styled.div`
+  position: relative;
+  width: 58%;
+  top: 97px;
+  left: 147px;
+  height: 69.3%;
 `;
