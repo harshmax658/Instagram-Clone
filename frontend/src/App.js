585c627ac9487a84c2bcc57489336088db2c3202
harshmax658 <harshmax658@gmail.com>
2022-01-29 12:40:36 +0530

    install react router dom v6 and setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import LoginSignupPage from "./pages/LoginSignupPage";
 import Header from "./components/header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AppStyle, GlobalStyleCss } from "./AppStyle";
 
 function App() {
   return (
-    <>
-      {/* Add Component inside App Block */}
+    <BrowserRouter>
       <AppStyle>
-        <Header />
-        {/* <LoginSignupPage /> */}
+        <Routes>
+          {/* Add Component inside App Block */}
+          <Route path="/" exact element={<Header />} />
+          <Route path="/login" element={<LoginSignupPage />} />
+          <Route path="*" element={<LoginSignupPage />} />
+        </Routes>
       </AppStyle>
       {/* Style  */}
       <GlobalStyleCss />
-    </>
+    </BrowserRouter>
   );
 }
 
