d7fc0424936f4d424ba90cc21b601adf47092dd1
harshmax658 <harshmax658@gmail.com>
2022-01-31 11:01:28 +0530

    remove conflicts

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,33 +1,32 @@
-
-import React from 'react';
-import { Container , Center , Image , Input , Left , Right} from './HeaderStyle';
-import LinkItem from './LinkItem';
-import HomeIcon from '@mui/icons-material/Home';
-import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import React from "react";
+import { Container, Center, Image, Input, Left, Right } from "./HeaderStyle";
+import LinkItem from "./LinkItem";
+import HomeIcon from "@mui/icons-material/Home";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const Header = () => {
   return (
-      <>
-        <Container>
-            <Left>
-                <Image src='https://hemsingh780.github.io/hosted-assest/instagram.png' />
-            </Left>
-            <Center>
-               <Input placeholder='search'/>
-            </Center>
-            <Right>
-                <LinkItem Icon = {<HomeIcon />} />
-                <LinkItem Icon = {<MessageOutlinedIcon /> } />
-                <LinkItem Icon = {<AddBoxOutlinedIcon /> } />
-                <LinkItem Icon = {<FavoriteBorderIcon /> } />
-                <LinkItem Icon = {<AccountCircleOutlinedIcon /> } /> 
-            </Right>
-        </Container>
-      </>
-    );
+    <>
+      <Container>
+        <Left>
+          <Image src="https://hemsingh780.github.io/hosted-assest/instagram.png" />
+        </Left>
+        <Center>
+          <Input placeholder="search" />
+        </Center>
+        <Right>
+          <LinkItem Icon={<HomeIcon />} />
+          <LinkItem Icon={<MessageOutlinedIcon />} />
+          <LinkItem Icon={<AddBoxOutlinedIcon />} />
+          <LinkItem Icon={<FavoriteBorderIcon />} />
+          <LinkItem Icon={<AccountCircleOutlinedIcon />} />
+        </Right>
+      </Container>
+    </>
+  );
 };
 
 export default Header;
