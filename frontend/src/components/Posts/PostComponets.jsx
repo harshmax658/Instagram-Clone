addab6f37a576ab0f367d2eb419768c2d2a5d549
harshmax658 <harshmax658@gmail.com>
2022-01-31 11:23:11 +0530

    add post page v1

---
+++ frontend/src/components/Posts/PostComponets.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { PostComponent } from "./PostStyle";
+
+const PostComponets = () => {
+  return <PostComponent>User Post</PostComponent>;
+};
+
+export default PostComponets;
---
+++ frontend/src/pages/Post Page/PostPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserPost } from "./PostPageStyle";
+import PostComponets from "../../components/Posts/PostComponets";
+const PostPage = () => {
+  return (
+    <UserPost>
+      <PostComponets />
+    </UserPost>
+  );
+};
+
+export default PostPage;
