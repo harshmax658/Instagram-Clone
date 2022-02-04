052250ef90a66ee35c6f3d452f84b706debdec1a
harshmax658 <harshmax658@gmail.com>
2022-02-04 22:09:13 +0530

    setup folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("firs2st");
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const routerPath = path.join(__dirname, "/routes");
+app.use(require(routerPath));
+
+app.listen(8000, () => {
+  console.log("Server start");
+});
---
+++ backend/routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/first", (req, res) => {
+  return res.status(200).json({
+    msg: "jai shree ram",
+  });
+});
+
+module.exports = router;
