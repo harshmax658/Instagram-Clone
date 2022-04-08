fc07b67ac8dde219b8c4fe0213a9b9b7ec04d7f8
harshmax658 <harshmax658@gmail.com>
2022-04-08 15:22:30 +0530

    add users controller

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    city: {
+      type: String,
+      max: 20,
+    },
+
+    desc: {
+      type: String,
+      max: 50,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
