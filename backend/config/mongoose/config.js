bf366869096d560506f34e43afdc377517dc7e3d
harshmax658 <harshmax658@gmail.com>
2022-04-08 13:37:16 +0530

    setupe backend and connect with Atlas DB

---
+++ backend/config/mongoose/config.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const url = `mongodb+srv://harshhem658:jaishreeram@cluster0.xs2qs.mongodb.net/instagram?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("Server configuration setup Succesfully....."))
+  .catch(() => {
+    console.log("Oops.. There is an error in db Config");
+  });
